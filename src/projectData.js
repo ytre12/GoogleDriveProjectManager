b5ac9c53c61b7ac1a02d64975370846bd3ac4e4d
Serhii Kanzeba <ytre@MacBook-Air-Serhii.local>
2026-07-09 18:12:04 +0300

    Add project 1

--- src/projectData.js
+++ src/projectData.js
@@ -1,59 +1,11 @@
 const DATA = [
 	{
-		id: 7,
+		id: 1,
 		name: 'LWork_13',
 		status: 'In Progress', 
 		data: '8.07.26',
 		linkToPortfolio: '-', 
 		linkToFull: 'https://drive.google.com/drive/folders/1nhn6laoOzuI-gQW4Bfqa4fiTZfiFjZc2?usp=sharing'
-	},
-	{
-		id: 1,
-		name: 'Project 3',
-		status: 'Complited', 
-		data: '12.05.26',
-		linkToPortfolio: 'https://pomofocus.io/', 
-		linkToFull: 'https://pomofocus.io/'
-	},
-	{
-		id: 2,
-		name: 'Project 2',
-		status: 'In Progress', 
-		data: '12.15.26',
-		linkToPortfolio: 'https://pomofocus.io/', 
-		linkToFull: 'https://pomofocus.io/'
-	},
-	{
-		id: 3,
-		name: 'Test Draft Project',
-		status: 'Draft', 
-		data: '67.05.67',
-		linkToPortfolio: 'https://pomofocus.io/', 
-		linkToFull: 'https://pomofocus.io/'
-	},
-	{
-		id: 4,
-		name: '1111111',
-		status: 'Draft', 
-		data: '67.05.67',
-		linkToPortfolio: 'https://pomofocus.io/', 
-		linkToFull: 'https://pomofocus.io/'
-	},
-	{
-		id: 5,
-		name: '901',
-		status: 'Draft', 
-		data: '67.05.67',
-		linkToPortfolio: 'https://pomofocus.io/', 
-		linkToFull: 'https://pomofocus.io/'
-	},
-	{
-		id: 6,
-		name: 'AAAaaaAAA',
-		status: 'Draft', 
-		data: '67.05.67',
-		linkToPortfolio: 'https://pomofocus.io/', 
-		linkToFull: 'https://pomofocus.io/'
 	},
 ]
 
