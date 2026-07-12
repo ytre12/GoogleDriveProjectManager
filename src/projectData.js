9db66f681e7bf8769eb739c901d978e39c9e4597
Serhii Kanzeba <ytre@MacBook-Air-Serhii.local>
2026-07-12 13:31:19 +0300

    Add new two progetc

--- src/projectData.js
+++ src/projectData.js
@@ -2,11 +2,29 @@ const DATA = [
   {
     id: 1,
     name: "LWork_13",
-    status: "In Progress",
+    status: "Complited",
     data: "8.07.26",
     linkToPortfolio: "",
     linkToFull:
       "https://drive.google.com/drive/folders/1nhn6laoOzuI-gQW4Bfqa4fiTZfiFjZc2?usp=sharing",
+  },
+  {
+    id: 2,
+    name: "LWork_14",
+    status: "In Progress",
+    data: "12.07.26",
+    linkToPortfolio: "",
+    linkToFull:
+      "https://drive.google.com/drive/folders/1JcRxmjuqymo6auiAWDsCwuY7Pq9dJuVj?usp=sharing",
+  },
+  {
+    id: 3,
+    name: "LWork_15",
+    status: "In Progress",
+    data: "12.07.26",
+    linkToPortfolio: "",
+    linkToFull:
+      "https://drive.google.com/drive/folders/1wWqM12SoU6bNhyMRsL9eMGZt2cC1anPM?usp=sharing",
   },
 ];
 
